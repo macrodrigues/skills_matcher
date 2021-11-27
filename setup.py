d014a99fc00c469903dfd16734e65af2d3d56e79
Marco Rodrigues <marcoacavaco.rodrigues@hotmail.com>
2021-11-27 20:13:29 +0000

    'initialcommit'

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(name='skills_matcher',
+      version="1.0",
+      description="Project Description",
+      packages=find_packages(),
+      install_requires=requirements,
+      test_suite='tests',
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      scripts=['scripts/skills_matcher-run'],
+      zip_safe=False)
